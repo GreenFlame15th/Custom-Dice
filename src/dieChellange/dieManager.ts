2e492b4444f966c9a5001d99dd83451dad1f8880
GreenFlame <58226448+GreenFlame15th@users.noreply.github.com>
2026-04-19 15:11:07 +0200

    update ui

--- src/dieChellange/dieManager.ts
+++ src/dieChellange/dieManager.ts
@@ -1,10 +1,12 @@
-import { AdvancedDynamicTexture, Control, Rectangle, TextBlock } from "@babylonjs/gui";
+import { AdvancedDynamicTexture, Control, Rectangle, StackPanel, TextBlock } from "@babylonjs/gui";
 import { Die } from "./die";
 import { Color3, Scene } from "@babylonjs/core";
 
 export class DieManager {
     public dice: Die[] = [];
-    private sumText!: TextBlock;
+    private sumText!: TextBlock;  
+    private scoreText!: TextBlock;
+    private bestScoreText!: TextBlock;
 
     public setUpDice(scene : Scene)
     {
@@ -34,8 +36,8 @@ export class DieManager {
         const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
         const panel = new Rectangle();
-        panel.width = "220px";
-        panel.height = "80px";
+        panel.width = "250px";
+        panel.height = "120px"; // Increased height for 3 lines
         panel.cornerRadius = 10;
         panel.color = "white";
         panel.background = "rgba(0,0,0,0.6)";
@@ -45,10 +47,31 @@ export class DieManager {
         panel.left = "-20px";
         advancedTexture.addControl(panel);
 
+        const stackPanel = new StackPanel();
+        panel.addControl(stackPanel);
+
         this.sumText = new TextBlock();
         this.sumText.text = "Sum: 0";
         this.sumText.color = "yellow";
-        this.sumText.fontSize = 28;
-        panel.addControl(this.sumText);
+        this.sumText.fontFamily = "Arial";
+        this.sumText.fontSize = 24;
+        this.sumText.height = "35px";
+        stackPanel.addControl(this.sumText);
+
+        this.scoreText = new TextBlock();
+        this.scoreText.text = "Score: 0";
+        this.scoreText.color = "#00ff00";
+        this.scoreText.fontFamily = "Arial";
+        this.scoreText.fontSize = 24;
+        this.scoreText.height = "35px";
+        stackPanel.addControl(this.scoreText);
+
+        this.bestScoreText = new TextBlock();
+        this.bestScoreText.text = "Best score: 0";
+        this.bestScoreText.color = "red";
+        this.bestScoreText.fontFamily = "Arial";
+        this.bestScoreText.fontSize = 24;
+        this.bestScoreText.height = "35px";
+        stackPanel.addControl(this.bestScoreText);
     }
 }
